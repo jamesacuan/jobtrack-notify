2781dddeb3154be351855ee7066343f29d02548e
jamesacuan <jec@sacuan.com>
2017-03-18 17:33:27 +0800

    added favicon support

--- js/notify.js
+++ js/notify.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name        jobtrack notify
-// @namespace   sencor.net
-// @include     http://jobtrack.ncg.sencor.net/resolverdb.aspx
-// @require     http://momentjs.com/downloads/moment.min.js
-// @version     1
-// @grant       none
-// ==/UserScript==
-
 moment().format();
 if (document.getElementById('ctl00_ContentPlaceHolder1_gvPending') === null) {
   checkToast();
@@ -23,8 +14,12 @@ else {
   title = 'Jobtrack #' + cells[0].textContent.replace(/\s+/g, ' ');
   time = moment(cells[2].textContent.replace(/\s+/g, ' ').trim(), 'MM/DD/YY hh:mm a').fromNow();
   body = cells[4].textContent.replace(/\s+/g, ' ') + '(' + cells[5].textContent.replace(/\s+/g, ' ').trim() + ')' + ' : ' + cells[1].textContent.replace(/\s+/g, ' ').trim() + '\n ' + time;
-  if (status == 'New') {
+  if (status == 'Pending') {
     toast(title, body);
+    setFavicon();
+  }
+  else{
+    unsetFavicon();
   }
 }
 function checkToast() {
@@ -38,6 +33,7 @@ function checkToast() {
     });
   }
 }
+
 function toast(title, body) {
   var notification = new Notification(title, {
     icon: 'http://eprofile.int.sencor.net/images/sencor150.png',
@@ -46,4 +42,22 @@ function toast(title, body) {
   notification.onclick = function () {
     window.focus();
   }
+}
+
+function setFavicon() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    /*i just don't know how to call the image, webextension uses isolated world to segregate 
+    the extension and the site.*/
+    link.href = 'https://raw.githubusercontent.com/jamesacuan/jobtrack-notify/master/img/favicon-new.png';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+
+function unsetFavicon(){
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://raw.githubusercontent.com/jamesacuan/jobtrack-notify/master/img/favicon-none.png';
+    document.getElementsByTagName('head')[0].appendChild(link);
 }

bd25788d519ea0483732c3b3bfcb28df2d11904c
James Earl C. Sacuan <jecsacuan@sencorhq.net>
2017-03-18 15:24:04 +0800

    changes to notify.js in prep for webextension

--- js/notify.js
+++ js/notify.js
@@ -2,11 +2,11 @@
 // @name        jobtrack notify
 // @namespace   sencor.net
 // @include     http://jobtrack.ncg.sencor.net/resolverdb.aspx
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     http://momentjs.com/downloads/moment.min.js
 // @version     1
 // @grant       none
 // ==/UserScript==
+
 moment().format();
 if (document.getElementById('ctl00_ContentPlaceHolder1_gvPending') === null) {
   checkToast();
@@ -23,7 +23,7 @@ else {
   title = 'Jobtrack #' + cells[0].textContent.replace(/\s+/g, ' ');
   time = moment(cells[2].textContent.replace(/\s+/g, ' ').trim(), 'MM/DD/YY hh:mm a').fromNow();
   body = cells[4].textContent.replace(/\s+/g, ' ') + '(' + cells[5].textContent.replace(/\s+/g, ' ').trim() + ')' + ' : ' + cells[1].textContent.replace(/\s+/g, ' ').trim() + '\n ' + time;
-  if (status == 'New') {
+  if (status == 'In-progress') {
     toast(title, body);
   }
 }

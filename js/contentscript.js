246aaba857bd56c425cec0e9a21ba1596a7133bb
jamesacuan <jec@sacuan.com>
2017-03-18 16:07:29 +0800

    changes for chrome compatibility, removed jquery

---
+++ js/contentscript.js
@@ -0,0 +1,12 @@
+var s = document.createElement('script');
+var t = document.createElement('script');
+s.src = chrome.extension.getURL('/js/moment.min.js');
+t.src = chrome.extension.getURL('/js/notify2.js');
+(document.head||document.documentElement).appendChild(s);
+(document.head||document.documentElement).appendChild(t);
+s.onload = function() {
+    s.parentNode.removeChild(s);
+};
+t.onload = function() {
+    t.parentNode.removeChild(t);
+};
